96fa3e00c7bf44b5aca0d684564856dd51c465c7
Fernando <fer_f@outlook.com>
2024-12-24 20:45:48 -0600

    getMessaging(app)

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -17,7 +17,7 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// export const messaging = getMessaging(app);
+export const messaging = getMessaging(app);
 
 
 // export const generateToken = async ()=>{
