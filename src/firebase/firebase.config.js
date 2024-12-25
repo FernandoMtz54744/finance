2a4b3e71a5c9d1d963c5dc5720deb5fea920a3f8
Fernando <fer_f@outlook.com>
2024-12-24 20:30:47 -0600

    Off genToken config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -17,15 +17,15 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const messaging = getMessaging(app);
+// export const messaging = getMessaging(app);
 
 
-export const generateToken = async ()=>{
-  const permission = await Notification.requestPermission();
-  if(permission === "granted"){
-    const token = await getToken(messaging, {
-      vapidKey: "BLU9RO4kUTgZA9w_Ok9NwDZhYWi_-igV1Dy54zQJrSRzDcYXuYliTgim6Jy3WF2hxGuxuRp1BjDZ30DCDd17SBk"
-    })
-    console.log(token)
-  }
-}
+// export const generateToken = async ()=>{
+//   const permission = await Notification.requestPermission();
+//   if(permission === "granted"){
+//     const token = await getToken(messaging, {
+//       vapidKey: "BLU9RO4kUTgZA9w_Ok9NwDZhYWi_-igV1Dy54zQJrSRzDcYXuYliTgim6Jy3WF2hxGuxuRp1BjDZ30DCDd17SBk"
+//     })
+//     console.log(token)
+//   }
+// }
